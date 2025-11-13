6e139acbc1eeda21c3ccf4ebaf32f2085d1b31e1
TaiNguyen <ngyntantai76@gmail.com>
2025-11-13 18:01:49 +0700

    chuan bi deloy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,26 @@ import taskRoutes from './routes/taskRoutes.js';
 import { connectDB } from '../config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173' }));
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors({ origin: 'http://localhost:5173' }));
+}
 
 app.use('/api/task', taskRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
+}
 
 connectDB().then(() => {
   app.listen(5001, () => {
